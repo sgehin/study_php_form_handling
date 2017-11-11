4ab638256dda359aae2a751afdd8481f30a5c273
sgehin <sfa.gehin@gmail.com>
2017-11-11 12:02:10 +0100

    create form in php

--- index.php
+++ index.php
@@ -4,14 +4,21 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php // formtest.php
+echo <<<_END
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="formtest.php" method="post">
+            what is your name?
+            <input type="text" name="name">
+            <input type="submit">
+        </form>
     </body>
 </html>
+_END;
+?>
+
