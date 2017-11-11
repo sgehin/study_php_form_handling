4c8255caa2327bf16885e0a35d739d493a728572
sgehin <sfa.gehin@gmail.com>
2017-11-11 14:37:45 +0100

    radio buttons & default checked

--- index.php
+++ index.php
@@ -6,17 +6,9 @@
     </head>
     <form method="post" action="calc.php">
         <pre>
-                  Remarks <textarea name="principle" cols="21" rows="4" wrap="type">type your question here.</textarea>
-
-                  I agree <input type="checkbox" name="agree" value="value" checked="checked">  <!--default checked setting -->
-              don't agree <input type="checkbox" name="dontAgree">  <!--no default checked setting -->
-
-        <!-- Submitting multiple values with an array :add [] to name all choice are submitted--> 
-
-                  Vanilla <input type="checkbox" name="ice[]">
-                Chocolate <input type="checkbox" name="ice[]">
-               Strawberry <input type="checkbox" name="ice[]">
-        <!-- To substrac the array use: foreach($ice as $item) echo "$item<br>";--> 
+                 yello <input type="radio" name="color" value="1">
+                   red <input type="radio" name="color" value="2">
+                 green <input type="radio" name="color" value="3" checked="checked">
 
                           <input type="submit" name="submit">
     </form>
