946c1f16a1c35826fc570a8f896c59b5d5afc1e7
sgehin <sfa.gehin@gmail.com>
2017-11-11 13:56:25 +0100

    input default values and organize with <pre>

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<?php // formtest.php
-if (isset($_post['name'])) $name = $_post['name'];
-else $name = "(Not entered)";
-
-echo <<<_END
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>index</title>
+        <title>Default Values</title>
+        
     </head>
-    <body>
-        Your name is: $name<br>
-        <form action=formtest.php method="post">
-            what is your name?
-            <input type="text" name="name">
-            <input type="submit">
-        </form>
-    </body>
+    <form method="post" action="calc.php">
+        <pre>
+              Loan Amount <input type="text" name="principle">
+        Monthly Repayment <input type="text" name="monthly">
+          Number of Years <input type="text" name="years" value="25">
+            Interest Rate <input type="text" name="rate" value="6">
+
+                          <input type="submit" name="submit">
+    </form>
+           
 </html>
-_END;
-?>
-
-<!--//if(true){}else{}
-
-//if () $name=jojo; else$name="(not jojo)"
